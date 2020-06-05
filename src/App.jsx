5db0316e702b2f3a4b0aeb4eef15535b238c328b
Hao Chen <chenhao@microduino.cc>
2020-06-05 17:29:22 +0800

    fix reoute issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import {Layout} from 'antd'
 import {HttpLink} from 'apollo-link-http'
 import {ApolloClient} from 'apollo-client'
